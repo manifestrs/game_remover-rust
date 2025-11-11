5f1a3ccbf88a74193bdca18f90fe086f9f659239
pastadudes <contact@pastaya.net>
2025-11-11 10:18:56 +0000

    move to ratatui

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,6 @@
 use std::path::PathBuf;
 
-use clap::Parser;
-
 #[tokio::main]
 async fn main() {
     println!("testing...");
-}
-
-#[derive(Parser, Debug)]
-struct Args {
-    steam_path: PathBuf,
 }
